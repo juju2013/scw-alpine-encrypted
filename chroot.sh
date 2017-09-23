669f3b36e8fcf136e58a7dd01bb59c5af8afa816
juju2013 <catherinedesienne@gmail.com>
2017-09-23 23:11:34 +0200

    working V1

--- chroot.sh
+++ chroot.sh
@@ -26,13 +26,14 @@ cpAllBinaries `which partprobe`
 cpAllBinaries `which wget`
 cpAllBinaries `which bash`
 cpAllBinaries `which busybox`
+cpAllBinaries `which switch_root`
 #following ar busybox applets
-BUSYBOXAPP="blkid blockdev depmod fdisk findfs fsck fstrim getty halt hdparm hwclock init ip loadkmap lsmod mdev mkdosfs mkfs.vfat modinfo modprobe reboot route setconsole slattach switch_root sysctl"
+BUSYBOXAPP="blkid blockdev chroot depmod fdisk findfs fsck fstrim getty halt hdparm hwclock init ip loadkmap lsmod mdev mkdosfs mkfs.vfat modinfo modprobe reboot route setconsole slattach sysctl"
 for ba in $BUSYBOXAPP; do 
   cd $ROOTFS/sbin/
   ln -s /bin/busybox $ba
 done
-BUSYBOXAPP="cat chgrp chmod chown cp cpio date dd df dmesg echo false fdflush fgrep grep gunzip gzip hostname iostat kbd_mode kill ln ls lzop mkdir mknode mktemp more mount mountpoint mv netstat ping ping6 ps pwd rm rmdir sed setpriv sh sync touch true uname zcat"
+BUSYBOXAPP="cat chgrp chmod chown cp cpio date dd df dmesg echo false fdflush fgrep grep gunzip gzip hostname iostat kbd_mode kill ln ls lzop mkdir mknode mktemp more mount mountpoint mv netstat ping ping6 ps pwd rm rmdir sed setpriv sh sleep sync touch true uname zcat"
 for ba in $BUSYBOXAPP; do 
   cd $ROOTFS/bin/
   ln -s busybox $ba
